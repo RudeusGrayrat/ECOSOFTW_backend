fc9cd389ed784113736ec613ff409471d5da8d12
RudeusGrayrat <miguelnc2502@gmail.com>
2026-01-13 09:49:55 -0500

    estado de cliente

--- src/controllers/Comercial/Clientes/postCliente.js
+++ src/controllers/Comercial/Clientes/postCliente.js
@@ -12,14 +12,14 @@ const postCliente = async (req, res) => {
   } = req.body;
   try {
     if (!tipoCliente || !cliente || !numeroDocumento || !telefono) {
-      return res.status(400).json({ message: "Faltan datos obligatorios para crear el cliente" });
+      return res.status(400).json({ message: "Faltan datos obligatorios para crear el cliente", type: "Advertencia" });
     }
     const findCliente = await Comercial_Clientes.findOne({
       cliente: cliente,
       numeroDocumento: numeroDocumento,
     });
     if (findCliente) {
-      return res.status(409).json({ message: "El cliente con ese número de documento ya existe" });
+      return res.status(409).json({ message: "El cliente con ese número de documento ya existe", type: "Error" });
     }
     const nuevoCliente = new Comercial_Clientes({
       tipoCliente,
@@ -29,11 +29,12 @@ const postCliente = async (req, res) => {
       telefono,
       correoElectronico,
       direccionLegal,
+      estado: "ACTIVO",
     })
     await nuevoCliente.save();
-    res.status(201).json({ message: "Cliente creado exitosamente", data: nuevoCliente });
+    res.status(201).json({ message: "Cliente creado exitosamente", data: nuevoCliente, type: "Correcto" });
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({ message: error.message, type: "Error" });
   }
 };
 
