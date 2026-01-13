fc857c819e7b0b36713de3df8225bc054e4084eb
RudeusGrayrat <miguelnc2502@gmail.com>
2026-01-13 10:41:24 -0500

    estado de cliente

--- src/Models/Comercial/Clientes.js
+++ src/Models/Comercial/Clientes.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 
 const comercial_clientesSchema = mongoose.Schema(
-  {
-    tipoCliente: {
-      type: String,
-      enum: ["EMPRESA", "PERSONA"],
-      required: true,
-    },
-    cliente: { type: String, required: true },
-    numeroDocumento: { type: String, required: true },
-    nombreContacto: { type: String },
+    {
+        tipoCliente: {
+            type: String,
+            enum: ["EMPRESA", "PERSONA"],
+            required: true,
+        },
+        cliente: { type: String, required: true },
+        numeroDocumento: { type: String, required: true },
+        nombreContacto: { type: String },
 
-    telefono: { type: String },
-    correoElectronico: { type: String },
+        telefono: { type: String },
+        correoElectronico: { type: String },
 
-    direccionLegal: { type: String },
-    estado: {
-      type: String,
-      enum: ["ACTIVO", "INACTIVO"],
-      default: "ACTIVO",
-      required: true,
+        direccionLegal: { type: String },
+        estado: {
+            type: String,
+            enum: ["ACTIVO", "INACTIVO"],
+            default: "ACTIVO",
+            required: true,
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Comercial_Clientes = mongoose.model(
-  "comercial_clientes",
-  comercial_clientesSchema
+    "comercial_clientes",
+    comercial_clientesSchema
 );
 
 module.exports = Comercial_Clientes;
